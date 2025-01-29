4c5fb766906b3c178379cb455fd665e159190fe2
KyleighS <111814352+KyleighS@users.noreply.github.com>
2025-01-29 18:42:26 -0500

    flashing rec dot

--- Assets/Scripts/ColorFlash.cs
+++ Assets/Scripts/ColorFlash.cs
@@ -4,16 +4,47 @@ using UnityEngine.UI;
 public class ColorFlash : MonoBehaviour
 {
     public Image dot;
-    public float speed = 1.0f;
-    void Start()
-    {
-        dot = GetComponent<Image>();
-        var tempColorAlpha = dot.color;
-        tempColorAlpha.a = 1f;
-        dot.color = tempColorAlpha;
-    }
+    public Color newColor;
+    public Color ogColor;
+    public float fadeTime = 0.1f;
+    public bool decrease = true;
+
     private void Update()
     {
-        
+        Color tempColor = dot.color;
+        if (dot.color != newColor && tempColor.a>0 && decrease  )
+        {
+            //Debug.Log("Increasing" + dot.color.a);
+            tempColor.a = tempColor.a - (float)0.01;
+            dot.color = tempColor;
+        }
+        else
+        {
+            //Debug.Log("SWAP" + dot.color);
+            decrease = false;
+        }
+        if (dot.color != ogColor && tempColor.a < 1 && !decrease)
+        {
+            //Debug.Log("Decreasing" + dot.color.a);
+            tempColor.a = tempColor.a + (float)0.01;
+            dot.color = tempColor;
+        }
+        else
+        {
+            //Debug.Log("Decreasing"+ dot.color);
+            decrease = true;
+        }
+
+    }
+
+    //this should be called somewhere in Update
+    void FadeOut()
+    {
+        dot.color = Color.Lerp(dot.color, newColor, fadeTime * Time.deltaTime);
+
+    }
+    void FadeIn()
+    {
+        dot.color = Color.Lerp(dot.color, ogColor, fadeTime * Time.deltaTime);
     }
 }
