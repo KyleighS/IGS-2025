a93ac46764656307f28d6f9307ae0309b5988e52
KyleighS <111814352+KyleighS@users.noreply.github.com>
2025-03-25 07:57:27 -0400

    Night 1 is set up the rest of the nights are duplicated will need to adjust thins

--- Assets/Scripts/Camera/CameraModes.cs
+++ Assets/Scripts/Camera/CameraModes.cs
@@ -44,17 +44,17 @@ public class CameraModes : MonoBehaviour
         }
 
         //activates thermal vision if the camera is up and F is pressed
-        if (Input.GetKeyDown(KeyCode.F) && camcorderOverlay.activeSelf)
-        {
-            SetNightVision(false, thermalCam.activeSelf);
-            thermalCam.SetActive(!thermalCam.activeSelf);
-        }
+        //if (Input.GetKeyDown(KeyCode.F) && camcorderOverlay.activeSelf)
+        //{
+        //    SetNightVision(false, thermalCam.activeSelf);
+        //    thermalCam.SetActive(!thermalCam.activeSelf);
+        //}
 
-        //for testing only, will be moved once we have a menu for it
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            Application.Quit();
-        }
+        ////for testing only, will be moved once we have a menu for it
+        //if (Input.GetKeyDown(KeyCode.Escape))
+        //{
+        //    Application.Quit();
+        //}
 
         //Reloads battery when R is pressed even if the camera is down
         if (Input.GetKeyDown(KeyCode.R))
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public string mainMenu = "MainMenu";
+    public GameObject pauseMenu;
+    public bool isPaused;
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+        isPaused = false;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+    }
+    public void PauseGame()
+    {
+        if (isPaused)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1f;
+            isPaused = false;
+        }
+        else
+        {
+            Cursor.lockState= CursorLockMode.None;
+            Cursor.visible = true;
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+            isPaused = true;
+        }
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(mainMenu);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+
+    }
+}
