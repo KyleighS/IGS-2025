1b1ee581f6d92100f1c33e144e5928f0233b7818
KyleighS <111814352+KyleighS@users.noreply.github.com>
2025-04-24 14:28:11 -0400

    Revert "hope this fixes things"
    This reverts commit a709b4ca527c811fc9d6021594e1ac1486561ec1.

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-
+using static UnityEditor.SceneView;
 
 public class DialogueManager : MonoBehaviour
 {
